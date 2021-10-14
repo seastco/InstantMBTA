5945fee2120a3e0f53d22acf7533f1f454860f8e
Ryan Angelo <RyanAngelo@users.noreply.github.com>
2021-10-13 21:27:04 -0400

    Updated with filters. Updated with notes. Need to filter based on time.

--- python/instant.py
+++ python/instant.py
@@ -8,54 +8,78 @@ print_headers = False
 
 def get_line(line_name):
     r = requests.get(API_URL+'/lines/'+line_name+'?'+API_REQUEST)
-    lines_json = r.json()
-    print(lines_json)
+    return r
 
-    if print_headers:
-        print(r.headers)
 
 def get_routes(route_id):
     r = requests.get(API_URL+'/routes/'+route_id+'?'+API_REQUEST)
-    lines_json = r.json()
-    print(lines_json)
-
-    if print_headers:
-        print(r.headers)
+    return r
 
 def get_schedule(route_id, stop_id, direction_id):
     r = requests.get(API_URL+'/schedules?include=stop&filter[route]='+\
         route_id+'&filter[stop]='+stop_id+'&filter[direction_id]='+direction_id+'&'+API_REQUEST)
-    lines_json = r.json()
-    print(lines_json)
-
-    if print_headers:
-        print(r.headers)
+    return r
 
 def get_predictions(stop_id, direction_id):
     r = requests.get(API_URL+'/predictions?filter[stop]='+stop_id+'&filter[direction_id]='+direction_id+'&include=stop&'+API_REQUEST)
-    lines_json = r.json()
-    print(lines_json)
-
-    if print_headers:
-        print(r.headers)
+    return r
 
 def get_stops(route_id):
     r = requests.get(API_URL+'/stops?filter[route]='+route_id+'&'+API_REQUEST)
-    lines_json = r.json()
-    print(lines_json)
+    return r
 
-    if print_headers:
-        print(r.headers)
+def get_current_schedule():
+    #Start by getting the schedule for a given stop and direction
+    #Outbound to Melrose Highlands
+    outbound_r = get_schedule('CR-Haverhill', 'place-WR-0075', '1')
+    outbound_json = outbound_r.json()
+    if len(outbound_json['data']) > 0:
+        next_outbound = outbound_json['data'][0] #Sorted by date/time
+        next_outbound_arrival_time = next_outbound['attributes']['arrival_time']
+        next_outbound_departure_time = next_outbound['attributes']['departure_time']
+    else:
+        #TODO: Handle empty
+        pass
+
+    #Start by getting the schedule for a given stop and direction
+    #Outbound to Melrose Highlands
+    outbound_pred_r = get_predictions('place-WR-0075', '1')
+    outbound_pred_json = outbound_pred_r.json()
+    if len(outbound_pred_json['data']) > 0:
+        next_outbound_pred = outbound_pred_json['data'][0] #Sorted by date/time
+        next_outbound_pred_arrival_time = next_outbound_pred['attributes']['arrival_time']
+        next_outbound_pred_departure_time = next_outbound_pred['attributes']['departure_time']
+    else:
+        #TODO: Handle empty
+        pass
+
+    #Inbound to North Station
+    inbound_r = get_schedule('CR-Haverhill', 'place-WR-0075', '0')
+    inbound_json = inbound_r.json()
+    if len(inbound_json['data']) > 0:
+        next_inbound = inbound_json['data'][0] #Sorted by date/time
+        next_inbound_arrival_time = next_inbound['attributes']['arrival_time']
+        next_inbound_departure_time = next_outbound['attributes']['departure_time']
+    else:
+        #TODO: Handle empty
+        pass
+
+
+    print(next_outbound_arrival_time)
+    print(next_inbound_arrival_time)
 
 if __name__ == '__main__':
+    #lines_json = r.json()
     #get_line('line-Haverhill')
     #print("------")
     #get_schedule('CR-Haverhill')
     #print("------")
     #get_routes('CR-Haverhill')
     #get_stops('CR-Haverhill')
-    schedule_outbound_mh = get_schedule('CR-Haverhill', 'WR-0075-B', '0')
-    #outbound_predictions = get_predictions('WR-0075-B','0')
+    #schedule_outbound_mh = get_schedule('CR-Haverhill', 'place-WR-0075', '0')
+    #outbound_predictions = get_predictions('place-WR-0075','0')
 
-    #schedule_inbound_mh = get_schedule('CR-Haverhill', 'WR-0075-B', '1')
-    #inbound_predictions = get_predictions('WR-0075-B','1')
+    #schedule_inbound_mh = get_schedule('CR-Haverhill', 'place-WR-0075', '1')
+    #inbound_predictions = get_predictions('place-WR-0075','1')
+
+    get_current_schedule()
