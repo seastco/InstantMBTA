e3382840a44f841ce42eff6585f79684ddb16216
Ryan Angelo <RyanAngelo@users.noreply.github.com>
2021-10-26 21:13:28 -0400

    Continued making progress on schedules and predictions. Added a time bound for the schedule to only show items that are the current time or later.

--- python/instant.py
+++ python/instant.py
@@ -1,5 +1,6 @@
 import requests
 import secret_constants
+from datetime import datetime
 
 API_REQUEST = "api_key="+secret_constants.API_KEY
 API_URL = "https://api-v3.mbta.com"
@@ -16,8 +17,12 @@ def get_routes(route_id):
     return r
 
 def get_schedule(route_id, stop_id, direction_id):
+    curr_time = get_current_time()
+    hour = curr_time.hour
+    minute = curr_time.minute
     r = requests.get(API_URL+'/schedules?include=stop&filter[route]='+\
-        route_id+'&filter[stop]='+stop_id+'&filter[direction_id]='+direction_id+'&'+API_REQUEST)
+        route_id+'&filter[stop]='+stop_id+'&filter[direction_id]='+direction_id+
+        '&filter[min_time]='+str(hour)+':'+str(minute)+'&'+API_REQUEST)
     return r
 
 def get_predictions(stop_id, direction_id):
@@ -28,11 +33,15 @@ def get_stops(route_id):
     r = requests.get(API_URL+'/stops?filter[route]='+route_id+'&'+API_REQUEST)
     return r
 
+def get_current_time():
+    current_time = datetime.now().time()
+    return current_time
+
 def get_current_schedule():
-    #Start by getting the schedule for a given stop and direction
-    #Outbound to Melrose Highlands
-    outbound_r = get_schedule('CR-Haverhill', 'place-WR-0075', '1')
+    #Outbound to Melrose Highlands from North Station scheduled
+    outbound_r = get_schedule('CR-Haverhill', 'place-WR-0075', '0')
     outbound_json = outbound_r.json()
+    #print(outbound_json)
     if len(outbound_json['data']) > 0:
         next_outbound = outbound_json['data'][0] #Sorted by date/time
         next_outbound_arrival_time = next_outbound['attributes']['arrival_time']
@@ -41,9 +50,8 @@ def get_current_schedule():
         #TODO: Handle empty
         pass
 
-    #Start by getting the schedule for a given stop and direction
-    #Outbound to Melrose Highlands
-    outbound_pred_r = get_predictions('place-WR-0075', '1')
+    #Outbound to Melrose Highlands from North Station predictions
+    outbound_pred_r = get_predictions('place-WR-0075', '0')
     outbound_pred_json = outbound_pred_r.json()
     if len(outbound_pred_json['data']) > 0:
         next_outbound_pred = outbound_pred_json['data'][0] #Sorted by date/time
@@ -53,9 +61,10 @@ def get_current_schedule():
         #TODO: Handle empty
         pass
 
-    #Inbound to North Station
-    inbound_r = get_schedule('CR-Haverhill', 'place-WR-0075', '0')
+    #Inbound to North Station from Melrose Highlands scheduled
+    inbound_r = get_schedule('CR-Haverhill', 'place-WR-0075', '1')
     inbound_json = inbound_r.json()
+    print(inbound_json)
     if len(inbound_json['data']) > 0:
         next_inbound = inbound_json['data'][0] #Sorted by date/time
         next_inbound_arrival_time = next_inbound['attributes']['arrival_time']
@@ -64,9 +73,23 @@ def get_current_schedule():
         #TODO: Handle empty
         pass
 
+    #Inbound to North Station from Melrose Highlands predicted
+    inbound_pred_r = get_predictions('place-WR-0075', '1')
+    inbound_pred_json = inbound_pred_r.json()
+    print("---",inbound_pred_json['data'])
+    if len(inbound_pred_json['data']) > 0:
+        next_inbound_pred = inbound_pred_json['data'][0] #Sorted by date/time
+        next_inbound_pred_arrival_time = next_inbound_pred['attributes']['arrival_time']
+        next_inbound_pred_departure_time = next_inbound_pred['attributes']['departure_time']
+    else:
+        #TODO: Handle empty
+        pass
 
-    print(next_outbound_arrival_time)
-    print(next_inbound_arrival_time)
+    
+    #print(next_outbound_pred)
+    #print(next_inbound_pred)
+    #print(next_outbound_arrival_time)
+    #print(next_inbound_arrival_time)
 
 if __name__ == '__main__':
     #lines_json = r.json()
