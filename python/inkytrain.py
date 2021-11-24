c65ee17ddb41ff204d1d7396d4a6048439c97bb1
Ryan Angelo <RyanAngelo@users.noreply.github.com>
2021-11-24 10:07:08 -0500

    Updated so that new image is created each time.

--- python/inkytrain.py
+++ python/inkytrain.py
@@ -15,10 +15,11 @@ class InkyTrain():
         self.inky_display = auto(ask_user=True, verbose=True)
         self.inky_display.h_flip = True
         self.inky_display.v_flip = True
-        self.img = Image.new("P", (self.inky_display.WIDTH, self.inky_display.HEIGHT))
-        self.draw = ImageDraw.Draw(self.img)
 
     def draw_inbound_outbound(self, line, next_inbound_str, next_outbound_str):
+
+        self.img = Image.new("P", (self.inky_display.WIDTH, self.inky_display.HEIGHT))
+        self.draw = ImageDraw.Draw(self.img)
         
         if next_inbound_str != None:
             next_inbound_dt = datetime.fromisoformat(next_inbound_str)
@@ -40,6 +41,7 @@ class InkyTrain():
         x_line = 10
         y_line = 0
         self.draw.text((x_line, y_line), line_text, self.inky_display.BLACK, font_line)
+
         today = datetime.now()
         date_text = today.strftime("%m/%d/%y")
         w_date, h_date = font_date.getsize(date_text)
@@ -56,7 +58,7 @@ class InkyTrain():
         outbound_message = "Next Outbound: " + next_outbound
         w_outbound, h_outbound = font_times.getsize(outbound_message)
         x_outbound = 10
-        y_outbound = y_inbound + (h_inbound/2) + 2
+        y_outbound = y_inbound + (h_inbound/2) + 4
         self.draw.text((x_outbound, y_outbound), outbound_message, self.inky_display.BLACK, font_times)
 
         self.inky_display.set_image(self.img)
