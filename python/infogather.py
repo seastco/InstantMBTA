b98201b2b694ecf206df2ce8f9d2ad7642e3e7bf
Ryan Angelo <RyanAngelo@users.noreply.github.com>
2021-12-22 15:27:21 -0500

    Updated to handle exceptions that may arise when the system goes out to get the schedule. This could be due to a number of different types of connection exceptions. It will sleep for a minute and then try agian.

--- python/infogather.py
+++ python/infogather.py
@@ -142,19 +142,20 @@ def get_current_schedule(route_id, stop_id):
 
 """Primarily for testing without the inky display"""
 if __name__ == '__main__':
-    old_mh_niat = None
-    old_mh_noat = None
-    old_mh_nidt = None
-    old_mh_nodt = None
-    old_ns_nodt = None
+    old_mh_niat = old_mh_noat = old_mh_nidt = old_mh_nodt = old_ns_nodt = None
+    mh_niat = mh_noat = mh_nidt = mh_nodt = None
+    ns_niat = ns_noat = ns_nidt = ns_nodt = None
     route_id = 'CR-Haverhill'
     melrose_highlands = 'place-WR-0075'
     north_station = 'place-north'
     while True:
-        mh_niat, mh_noat, mh_nidt, mh_nodt = get_current_schedule(route_id, melrose_highlands)
-        ns_niat, ns_noat, ns_nidt, ns_nodt = get_current_schedule(route_id, north_station)
-        if (old_mh_niat != mh_niat or old_mh_noat != mh_noat or old_ns_nodt != ns_nodt):
-            print("Screen refresh activated")
+        try:
+            mh_niat, mh_noat, mh_nidt, mh_nodt = get_current_schedule(route_id, melrose_highlands)
+            ns_niat, ns_noat, ns_nidt, ns_nodt = get_current_schedule(route_id, north_station)
+            if (old_mh_niat != mh_niat or old_mh_noat != mh_noat or old_ns_nodt != ns_nodt):
+                print("Screen refresh activated")
+        except Exception as err:
+            print("There was an exception with the connection: {0}").format(err)
         print(mh_niat, mh_noat, mh_nidt, mh_nodt)
         print(ns_niat, ns_noat, ns_nidt, ns_nodt)
         print("-----")
--- python/instantmbta.py
+++ python/instantmbta.py
@@ -8,19 +8,20 @@ Inky e-ink display up-to-date
 """
 if __name__ == '__main__':
     it = InkyTrain()
-    old_mh_niat = None
-    old_mh_noat = None
-    old_mh_nidt = None
-    old_mh_nodt = None
-    old_ns_nodt = None
+    old_mh_niat = old_mh_noat = old_mh_nidt = old_mh_nodt = old_ns_nodt = None
+    mh_niat = mh_noat = mh_nidt = mh_nodt = None
+    ns_niat = ns_noat = ns_nidt = ns_nodt = None
     route_id = 'CR-Haverhill'
     melrose_highlands = 'place-WR-0075'
     north_station = 'place-north'
     while True:
-        mh_niat, mh_noat, mh_nidt, mh_nodt = get_current_schedule(route_id, melrose_highlands)
-        ns_niat, ns_noat, ns_nidt, ns_nodt = get_current_schedule(route_id, north_station)
-        if (old_mh_niat != mh_niat or old_mh_noat != mh_noat or old_ns_nodt != ns_nodt): 
-            it.draw_inbound_outbound("Haverhill", "Melrose Highlands", "North Station", mh_niat, mh_noat, ns_niat, ns_nodt)
+        try:
+            mh_niat, mh_noat, mh_nidt, mh_nodt = get_current_schedule(route_id, melrose_highlands)
+            ns_niat, ns_noat, ns_nidt, ns_nodt = get_current_schedule(route_id, north_station)
+            if (old_mh_niat != mh_niat or old_mh_noat != mh_noat or old_ns_nodt != ns_nodt): 
+                it.draw_inbound_outbound("Haverhill", "Melrose Highlands", "North Station", mh_niat, mh_noat, ns_niat, ns_nodt)
+        except Exception as err:
+            print("There was an exception with the connection: {0}").format(err)
         old_mh_niat = mh_niat
         old_mh_noat = mh_noat
         old_mh_nidt = mh_nidt
